d7d5fc4406c559b084bce2af8e317874d906a71d
szymon <skrupa@student.agh.edu.pl>
2022-07-22 23:50:18 +0200

    adding validation

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email")
-    password = PasswordField("Password")
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField(label="Log In")
 
 
@@ -18,9 +19,10 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
+    login_form.validate_on_submit()
     return render_template("login.html", form=login_form)
 
 
